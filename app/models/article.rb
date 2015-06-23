a8e112981eb40b7c1b11f43a9b576a0ef3fd92a6
Henrique Zambon <henrique.zambon@gmail.com>
2015-06-23 15:34:18 -0700

    More validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ActiveRecord::Base
 
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :text, length: { minimum: 10 }
+  validates :text, presence: true,
+                   length: { minimum: 10 }
 end
